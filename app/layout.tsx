f00e4faa1df7c4df18f5236f9a2e9dda5647b7b1
konumata <kota.numata@gmail.com>
2025-02-23 23:29:14 +0900

    feat: サイトのメタデータを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
 import React from "react"
 import './global.css'
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Portfolio | K - Engineer',
+  description: 'フルスタックエンジニアを目指すKのポートフォリオサイトです。Web開発、システム設計、AIなど幅広い技術スタックを活かしたプロジェクト実績を紹介しています。',
+};
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="ja">
-        <head>
-          <link rel="stylesheet" type="text/css" href="http://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/move02/4-1/css/reset.css"/>
-          <link rel="stylesheet" type="text/css" href="http://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/move02/4-1/css/4-1.css"/>
-        </head>
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <head>
+        <link rel="stylesheet" type="text/css" href="http://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/move02/4-1/css/reset.css"/>
+        <link rel="stylesheet" type="text/css" href="http://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/move02/4-1/css/4-1.css"/>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
